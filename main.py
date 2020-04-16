01cc927d9841794a4a78e03ce24d81ce0743dc4c
Atharva Pansare <61797592+Atharva7007@users.noreply.github.com>
2020-04-16 16:50:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ pygame.init()
 
 class Dino():
     def __init__(self):
-        self.Img = pygame.image.load("dino_.png")
+        self.Img = pygame.image.load("sprites/dino_.png")
         self.WIDTH, self.HEIGHT = 44, 48
         self.Img = pygame.transform.scale(self.Img, (self.WIDTH, self.HEIGHT))
         self.image = self.Img
@@ -17,13 +17,13 @@ class Dino():
         self.t = 0 # time
         self.hitbox = pygame.Rect(self.x + 5, self.y, self.WIDTH - 15, self.HEIGHT - 5)
 
-        self.runImg1 = pygame.image.load("dino_1.png")
-        self.runImg2 = pygame.image.load("dino_2.png")
+        self.runImg1 = pygame.image.load("sprites/dino_1.png")
+        self.runImg2 = pygame.image.load("sprites/dino_2.png")
         self.runImg1 = pygame.transform.scale(self.runImg1, (self.WIDTH, self.HEIGHT))
         self.runImg2 = pygame.transform.scale(self.runImg2, (self.WIDTH, self.HEIGHT))
 
-        self.duck1 = pygame.image.load("dino_ducking1.png")
-        self.duck2 = pygame.image.load("dino_ducking2.png")
+        self.duck1 = pygame.image.load("sprites/dino_ducking1.png")
+        self.duck2 = pygame.image.load("sprites/dino_ducking2.png")
         self.duck1 = pygame.transform.scale(self.duck1, (self.WIDTH + 15, self.HEIGHT))
         self.duck2 = pygame.transform.scale(self.duck2, (self.WIDTH + 15, self.HEIGHT))
         self.ducking = False
@@ -73,8 +73,8 @@ class Dino():
 class Ptera():
     def __init__(self):
         self.WIDTH, self.HEIGHT = 50, 40
-        self.im1 = pygame.image.load("ptera1.png")
-        self.im2 = pygame.image.load("ptera2.png")
+        self.im1 = pygame.image.load("sprites/ptera1.png")
+        self.im2 = pygame.image.load("sprites/ptera2.png")
 
         self.im1 = pygame.transform.scale(self.im1, (self.WIDTH, self.HEIGHT))
         self.im2 = pygame.transform.scale(self.im2, (self.WIDTH, self.HEIGHT))
@@ -117,8 +117,8 @@ class Ptera():
 
 class Cactus():
     def __init__(self):
-        self.image0 = pygame.image.load("cacti-small.png")
-        self.image1 = pygame.image.load("cacti-big.png")
+        self.image0 = pygame.image.load("sprites/cacti-small.png")
+        self.image1 = pygame.image.load("sprites/cacti-big.png")
         self.width0 = 45
         self.height = 44
         self.width1 = 50
@@ -164,10 +164,10 @@ class Cactus():
 class Ground():
     def __init__(self):
         self.ground_length = 1202
-        self.image1 = pygame.image.load("ground.png")
+        self.image1 = pygame.image.load("sprites/ground.png")
         self.image1_x = 0
         self.image1_y = 200
-        self.image2 = pygame.image.load("ground.png")
+        self.image2 = pygame.image.load("sprites/ground.png")
         self.image2_x = self.image1_x + self.ground_length
         self.image2_y = self.image1_y
         self.speed = 4
@@ -189,7 +189,7 @@ class Ground():
 class Cloud():
 
     def __init__(self):
-        self.image = pygame.image.load("cloud.png")
+        self.image = pygame.image.load("sprites/cloud.png")
         self.WIDTH, self.HEIGHT = 70, 40
         self.image = pygame.transform.scale(self.image, (self.WIDTH, self.HEIGHT))
         self.speed = 1
@@ -214,13 +214,13 @@ def game():
     check_point = pygame.mixer.Sound("checkPoint.wav")
     death_sound = pygame.mixer.Sound("die.wav")
     pygame.display.set_caption("Dino Run")
-    dino_icon = pygame.image.load("dino.png")
+    dino_icon = pygame.image.load("sprites/dino.png")
 
     pygame.display.set_icon(dino_icon)
 
-    game_over = pygame.image.load("game_over.png")
-    replay_button = pygame.image.load("replay_button.png")
-    logo = pygame.image.load("logo.png")
+    game_over = pygame.image.load("sprites/game_over.png")
+    replay_button = pygame.image.load("sprites/replay_button.png")
+    logo = pygame.image.load("sprites/logo.png")
     GREY = (240, 240, 240)
     WHITE = (255, 255, 255)
 
